aef1258d004d78e724624aba3b355de447bd7c6e
Erik <152098698+ErikG008@users.noreply.github.com>
2025-04-24 18:53:28 -0300

    Create questão 01.py

---
+++ Variáveis e Expressões Aritméticas/questão 01.py
@@ -0,0 +1,7 @@
+#programa que faça conversão cambial de dolar pra real, dolar em 5.27
+Dólar = 5.27
+
+dolares = float(input("Digite o valor em dólar: "))
+reais = dolares * Dólar
+
+print(f"O valor em reais é: {reais:.2f}")
